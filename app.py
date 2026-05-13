d269b3bbff23feaa57f6183d1e1360fc6c8eb225
mayy-z <mayzaw@shgcham.school.nz>
2026-05-13 12:58:21 +1200

    nav bar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from livereload import Server
 
 app = Flask(__name__)
 
@@ -8,4 +9,13 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Hot reload using live server
+    server = Server(app.wsgi_app)
+    server.watch("templates/")
+    server.watch("static/")
+    server.watch("static/")
+    server.serve(
+        port=5000,
+        liveport=35729,
+        debug=True
+    )
